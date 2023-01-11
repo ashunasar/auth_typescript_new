d22685809e2cebf05439e122ab43221e3a1d5b04
Asim Siddiqui <asimsiddiqui20172017@gmail.com>
2023-01-11 22:42:46 +0530

    soem changes

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import * as dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import createError from "http-errors";
+import { request } from "http";
+import AuthRouter from "./routes/auth.route";
+const app = express();
+
+dotenv.config();
+app.use(morgan("dev"));
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
+app.use("/auth", AuthRouter);
+
+app.use(async (req: Request, res: Response, next: NextFunction) => {
+  next(createError.NotFound());
+});
+
+app.use(async (err: any, req: Request, res: Response, next: NextFunction) => {
+  res.send({
+    error: {
+      statusCode: err.statusCode,
+      message: err.message,
+    },
+  });
+});
+app.listen(PORT, () => {
+  console.log(`server started at ${PORT}`);
+});
---
+++ src/routes/auth.route.ts
@@ -0,0 +1,20 @@
+import express, { NextFunction, Request, Response } from "express";
+const router = express.Router();
+
+router.post("/register", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello from register route");
+});
+
+router.post("/login", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello from login route");
+});
+router.post(
+  "/refresh-token",
+  (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello from refresh-token route");
+  }
+);
+router.delete("/logout", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello from logout route");
+});
+export default router;
